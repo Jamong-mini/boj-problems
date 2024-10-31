ea8bd48266756ee1d155050d3819d43a0c5a842a
Jamong-mini <jamong.alsl@gmail.com>
2024-10-31 09:38:03 +0900

    Delete Swift/프로그래머스/0/120820. 나이 출력 directory

--- Swift/프로그래머스/0/120820. 나이 출력/나이 출력.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-func solution(_ age:Int) -> Int {
-    guard (1...120).contains(age) else {
-        fatalError("제한사항: 0 < age <= 120")
-    }
-    
-    return 2022 + 1 - age
-}
